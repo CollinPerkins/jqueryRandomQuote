72348c5c12bbfddd0a3add971a7b3c36ee60f173
Collin Perkins <collinperkinsweb@gmail.com>
2017-07-20 01:15:29 -0500

    added javascript

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
-  // alert('hello');
+  getQuote();
 });
+
+var quoteObj = {
+  quote: "",
+  quoteAuthor: ""
+}
+
+$('.newQuote').click(function() {
+  getQuote();
+});
+
+function getQuote() {
+  $.ajax({
+      url: "http://api.forismatic.com/api/1.0/?",
+      dataType: "jsonp",
+      data: "method=getQuote&lang=en&format=jsonp&jsonp=?",
+      success: function( response ) {
+        quoteObj.quote = response.quoteText;
+        quoteObj.quoteAuthor = response.quoteAuthor;
+        insertHtml();
+      }
+  });
+};
+
+function insertHtml(){
+  $('.quote').html(quoteObj.quote);
+  $('.quoteAuthor').html("-" + quoteObj.quoteAuthor);
+  $('.twitterBtn').attr('href','https://twitter.com/intent/tweet?text=' + quoteObj.quote + '-' +  quoteObj.quoteAuthor );
+};
